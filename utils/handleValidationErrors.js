20325210a5bf9accfa31e2e677782f0fabf83032
Aleksandr Serikov <Sashas840@gmail.com>
2022-12-18 09:19:16 +0300

    Post validation fix

--- utils/handleValidationErrors.js
+++ utils/handleValidationErrors.js
@@ -2,6 +2,6 @@ import { validationResult } from 'express-validator';
 
 export default (req, res, next) => {
 	const err = validationResult(req);
-	if (!err.isEmpty()) return response.status(401).json(err.errors);
+	if (!err.isEmpty()) return res.status(401).json(err.errors);
 	next();
 };
