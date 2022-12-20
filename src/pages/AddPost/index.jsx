411108deb5fbcca05821e5df14b5cbd67b9164f4
Aleksandr Serikov <Sashas840@gmail.com>
2022-12-20 21:48:54 +0300

    Added some post creation form functionality & uploading image to the server

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -3,69 +3,129 @@ import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
+import { Navigate } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+	const isAuth = useSelector(selectIsAuth);
 
-  const handleChangeFile = () => {};
+	const [title, setTitle] = React.useState('');
+	const [tags, setTags] = React.useState('');
+	const [text, setText] = React.useState('');
+	const [imageUrl, setImageUrl] = React.useState('');
 
-  const onClickRemoveImage = () => {};
+	const inputFileRef = React.useRef(null);
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
-  }, []);
+	const handleChangeFile = async (event) => {
+		try {
+			const formData = new FormData();
+			formData.append('image', event.target.files[0]);
+			const { data } = await axios.post('/upload', formData, {
+				headers: { 'Content-Type': 'image' },
+			});
+			console.log(data.url);
+			setImageUrl(data.url);
+		} catch (err) {
+			console.warn(err);
+		}
+	};
 
-  const options = React.useMemo(
-    () => ({
-      spellChecker: false,
-      maxHeight: '400px',
-      autofocus: true,
-      placeholder: 'Введите текст...',
-      status: false,
-      autosave: {
-        enabled: true,
-        delay: 1000,
-      },
-    }),
-    [],
-  );
+	const onClickRemoveImage = () => {
+		setImageUrl('');
+	};
 
-  return (
-    <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
-        Загрузить превью
-      </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
-      {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
-      )}
-      <br />
-      <br />
-      <TextField
-        classes={{ root: styles.title }}
-        variant="standard"
-        placeholder="Заголовок статьи..."
-        fullWidth
-      />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
-      <div className={styles.buttons}>
-        <Button size="large" variant="contained">
-          Опубликовать
-        </Button>
-        <a href="/">
-          <Button size="large">Отмена</Button>
-        </a>
-      </div>
-    </Paper>
-  );
+	const onChange = React.useCallback((value) => {
+		setText(value);
+	}, []);
+
+	const options = React.useMemo(
+		() => ({
+			spellChecker: false,
+			maxHeight: '400px',
+			autofocus: true,
+			placeholder: 'Введите текст...',
+			status: false,
+			autosave: {
+				enabled: true,
+				delay: 1000,
+			},
+		}),
+		[]
+	);
+
+	if (!localStorage.getItem('token') && !isAuth) {
+		return <Navigate to={'/'} />;
+	}
+
+	return (
+		<Paper style={{ padding: 30 }}>
+			<Button
+				onClick={() => inputFileRef.current.click()}
+				variant="outlined"
+				size="large"
+			>
+				Загрузить превью
+			</Button>
+			<input
+				ref={inputFileRef}
+				type="file"
+				onChange={handleChangeFile}
+				hidden
+			/>
+			{imageUrl && (
+				<>
+					<Button
+						variant="contained"
+						color="error"
+						onClick={onClickRemoveImage}
+					>
+						Удалить
+					</Button>
+					<img
+						className={styles.image}
+						src={`http://localhost:4444${imageUrl}`}
+						alt="Uploaded"
+					/>
+				</>
+			)}
+			<br />
+			<br />
+			<TextField
+				classes={{ root: styles.title }}
+				variant="standard"
+				placeholder="Заголовок статьи..."
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+				fullWidth
+			/>
+			<TextField
+				classes={{ root: styles.tags }}
+				variant="standard"
+				placeholder="Тэги"
+				value={tags}
+				onChange={(e) => setTags(e.target.value)}
+				fullWidth
+			/>
+			<SimpleMDE
+				className={styles.editor}
+				value={text}
+				onChange={onChange}
+				options={options}
+			/>
+			<div className={styles.buttons}>
+				<Button size="large" variant="contained">
+					Опубликовать
+				</Button>
+				<a href="/">
+					<Button size="large">Отмена</Button>
+				</a>
+			</div>
+		</Paper>
+	);
 };
